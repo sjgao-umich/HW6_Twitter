53bf8d0d6b0c9545f4811e4c36ded72e3f8d69b5
sjgao-umich <70728140+sjgao-umich@users.noreply.github.com>
2021-03-30 03:50:18 +0800

    Delete secrets_starter_code.py

--- .gitignore/secrets_starter_code.py
+++
@@ -1,8 +0,0 @@
-#fill this in with your credentials and rename to secrets.py
-#make sure to gitignore this file! you will lose points if you share this in your github!
-
-TWITTER_API_KEY="dbdRgotzvMxOOCY1dJHpXw8Bg"
-TWITTER_API_SECRET="eZmRANDGE8zGFg5jEOwwKwglFR2OmcvKncSdPgJUqPTnOAE8oc"
-TWITTER_ACCESS_TOKEN="231115939-rJ7NkeXyCbBB0T7SWmvHAS49O9c4zyqp6iIVaKB4"
-TWITTER_ACCESS_TOKEN_SECRET="LV0sFAVK2dLpFwGNa1pSgLX57tkjoX50kXmC4LuJTfuAn"
-
